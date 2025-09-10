c80e9ea0ba472e656c6c40066677035fe1322f46
unknown <marcos.vinicius.taira@gmail.com>
2025-09-10 15:22:51 -0300

    primeira versao do projeto

---
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiMarcosApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpe.oxefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OxefoodApiMarcosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OxefoodApiMarcosApplication.class, args);
+	}
+
+}
