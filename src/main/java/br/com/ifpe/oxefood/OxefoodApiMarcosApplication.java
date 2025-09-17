6d28ca942c7e76306a8b193f1c22e9e0f447e8e9
adrijose1 <adrianojose10@gmail.com>
2025-09-17 17:08:37 -0300

    terceiro commit

--- src/main/java/br/com/ifpe/oxefood/OxefoodApiMarcosApplication.java
+++ src/main/java/br/com/ifpe/oxefood/OxefoodApiMarcosApplication.java
@@ -2,8 +2,11 @@ package br.com.ifpe.oxefood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing //professor vai explicar depois 
+
 public class OxefoodApiMarcosApplication {
 
 	public static void main(String[] args) {
